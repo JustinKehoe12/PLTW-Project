2a2145c0084386abfc2ee2c46b9d0a06486f665c
Justin Kehoe <47858778+JustinKehoe12@users.noreply.github.com>
2019-03-05 08:46:02 -0600

    Delete Hangman.py

--- Hangman.py
+++
@@ -1,7 +0,0 @@
-word_list = ['acres', 'adult', 'advice', 'arrangement', 'attempt', 'august','Autumn','border','breeze','brick','calm','canal','Casey','cast','chose','claws','coach','constantly'
-            ,'contrast','cookies','customs','damage','Danny','deeply','depth','discussion','doll','donkey','essential','exchange','exist','explanation',
-            'facing','film','finest','fireplace','floating','folks','fort','garage','grabbed','grandmother','habit','happily','heading','hunter',
-            'image','independent','instant','kids','label','lungs','manufacturing','mathematics','melted','memory','mill','mission','moneky','mount','mysterious','neighborhood',
-            'nuts','occasionally','official','ourselves','palace','plates','poetry','policemen','positive','possibly','practical','prode','promised','recall','relationship',
-            'remarkable','require','rhyme','rocky','rubbed','rush','sale','satellites','satisfied','selection','shake','shaking','shallow','shout','silly','simplest','slight',
-            'slip','slope','soap','solar','species','spin','stiff','swung','tales','thumb','tobacco','toy','trap','treated','vapor','vessels','wealth','wolf','zoo']
